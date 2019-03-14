041395404b50fa7493f98d21a83494df3f120971
suresh.thomas@yahoo.com <Leena@123>
2019-03-14 23:08:43 +0000

    sasasasa:wq

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,39 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider','$urlRouterProvider',function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home',{
+        url:'/',
+        templateUrl:'views/instructions.html'
+      })
+    .state('brick',{
+      url: '/bricks/:color',
+      templateUrl: 'views/bricks.html',
+      controllerProvider: function ($stateParams) {
+        var color = $stateParams.color;
+        color = color[0].toUpperCase()+ color.slice(1);
+        var ctrlName = color+'BricksCtrl';
+        return ctrlName;
+      },
+      controllerAs: 'brick'
+    })
+
+
+
+      .state('cart',{
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+
+      .state('brick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      });
+
+  }]);
